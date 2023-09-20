67cb3cbab06f081d6f76dda8720aab674ae5dee0
jeongchaeuk <jeongchaeuk@gmail.com>
2023-09-20 16:59:42 +0900

    Update quick_start.py

--- quick_start.py
+++ quick_start.py
@@ -9,18 +9,42 @@ def main(width=1280, height=720, fps=60):
     screen = pygame.display.set_mode((width, height))
     clock = pygame.time.Clock()
     running = True
+    delta_sec = 0
+    move_per_seconds = 300
+    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() /2)
 
+    # colors = list(pygame.color.THECOLORS.keys())
+    # color_index = 0
     while running:
         for event in pygame.event.get():
-            if event == pygame.QUIT:
+            # close window
+            if event.type == pygame.QUIT:
                 running = False
 
         screen.fill('black')
+        # screen.fill(colors[color_index])
+        # color_index += 1
+        # if color_index >= len(colors):
+        #     color_index = 0
 
         #render your game here.
+        pygame.draw.circle(screen, 'yellow', player_pos, 40)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            player_pos.y -= move_per_seconds * delta_sec
+        if keys[pygame.K_s]:
+            player_pos.y += move_per_seconds * delta_sec
+        if keys[pygame.K_a]:
+            player_pos.x -= move_per_seconds * delta_sec
+        if keys[pygame.K_d]:
+            player_pos.x += move_per_seconds * delta_sec
 
         pygame.display.flip()
-        clock.tick(fps)
+
+        # delta_sec is delta time in seconds since last frame, used for
+        # framerate-independent physic.
+        delta_sec = clock.tick(fps) / 1000
 
     pygame.quit()
 
